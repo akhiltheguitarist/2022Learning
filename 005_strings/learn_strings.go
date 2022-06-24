e8441721db9c540ab3b557a62660c06fb01a1f3d
Akhil <akhiltheguitarist@gmail.com>
2022-06-24 17:35:02 +0530

    Updated strings lesson

--- 005_strings/learn_strings.go
+++ 005_strings/learn_strings.go
@@ -9,6 +9,8 @@ func main() {
 	raw_string_invoke()
 	string_combine()
 	access_characters()
+	substringemoji()
+	arraycheck()
 }
 
 func raw_string_invoke() {
@@ -26,6 +28,24 @@ func string_combine() {
 func access_characters() {
 	s := "Hello world"
 	b := s[0]
-	c := s[5]
+	c := s[7]
 	fmt.Println("\n", s, b, string(b), c, string(c))
+	fmt.Printf("Length of string: %v", len(s))
+}
+
+func substringemoji() {
+	s := "😊❤️"
+	a := s[:1]
+	b := s[2:]
+	fmt.Println("\n\n", s, "\t", a, "\t", b)
+}
+
+func arraycheck() {
+	var myArr [2]string
+	myArr[0] = "10"
+	fmt.Println(myArr)
+
+	var myArrInt [2]int
+	myArrInt[0] = 10
+	fmt.Println(myArrInt)
 }
